f706135961e3679fbc91749d005c778a2099d859
Tome Hirata <tomu.hirata@gmail.com>
2020-12-09 23:40:48 +0900

    initialize

---
+++ lib/app.rb
@@ -0,0 +1,27 @@
+class App
+  def foo(n)
+    n.to_s
+  end
+
+  def boo(fn)
+    fn.call
+  end
+
+  def baz(fn)
+    fn.call(5)
+  end
+
+  def bar(&fn)
+    fn.call(0)
+  end
+end
+
+proc1 = proc { "ブロック" }
+proc2 = Proc.new { |n| n.to_s }
+
+app = App.new
+
+p app.foo(5)
+p app.boo(proc1)
+p app.baz(proc2)
+p app.bar { |a| a.to_s }
